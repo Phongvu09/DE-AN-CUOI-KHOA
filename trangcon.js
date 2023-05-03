a8cc466e5239d642b0ba2a4c28e412ae074021b6
unknown <vuphong28102009@gmail.cok>
2023-05-03 18:15:53 +0700

    solve conflicts

--- trangcon.js
+++ trangcon.js
@@ -8,20 +8,28 @@ const options = {
   },
 };
 
+const trangban = document.getElementById("trangban");
+
 fetch(`https://gamerpower.p.rapidapi.com/api/giveaway?id=${trang}`, options)
   .then((response) => response.json())
   .then((data) => {
     let card = document.createElement("div");
     let anh = document.createElement("img");
     let title = document.createElement("h5");
-    let trangban = document.getElementById("trangban");
+    let description = document.createElement("h7");
+    let gia = document.createElement("h6");
+
     card.classList.add("trang");
     anh.classList.add("img");
 
     anh.setAttribute("src", data.thumbnail);
+    description.innerText = data.description;
     title.innerText = data.title;
+    gia.innerText = data.worth;
 
     card.appendChild(anh);
     card.appendChild(title);
+    card.appendChild(description);
+    card.appendChild(gia);
     trangban.appendChild(card);
   });
